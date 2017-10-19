b916d01b28b7971049ff7563e47d56a872d464e5
Justin Welenofsky <welenofsky@gmail.com>
2017-10-19 13:08:08 -0700

    Turn HideEmails into module

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Hide Email
 this package will find all elements with the class h-email and it will replace contents of that tag with a canvas that has the email
 
-WIP - Do Not Use
+## Usage
+First create an element that has a data-hea attribute containing a base64 encoded email and a class of h-email
+```html
+<a href="#"><span class="email-styles h-email" data-hea="ZW1haWxAZXhhbXBsZS5jb20="></span></a>
+```
+Then use the HideEmail() function
+```javascript
+document.addEventListener("DOMContentLoaded", function(event) {
+    HideEmails();
+});
+```
+An example of how it is used is in the test.html file.
--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,38 @@
 const gulp = require('gulp'),
     babel = require('gulp-babel'),
-    path = require('path'),
     jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
     notify = require('gulp-notify');
 
 
-gulp.task('build', function() {
-  return gulp.src('./src/index.js')
-    .pipe(jshint())
-    .pipe(notify(function (file) {
-      if (file.jshint.success) {
-        return false;
-      }
+gulp.task('build', function () {
+    return gulp.src('./src/index.js')
+        .pipe(jshint())
+        .pipe(notify(function (file) {
+            if (file.jshint.success) {
+                return false;
+            }
 
-      var errors = file.jshint.results.map(function (data) {
-        if (data.error) {
-          return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
-        }
-      }).join("\n");
-      return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
-    }))
-    .pipe(babel({
-      presets: ['env']
-    }))
-    .pipe(gulp.dest('./dist'));
+            var errors = file.jshint.results.map(function (data) {
+                if (data.error) {
+                    return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+                }
+            }).join("\n");
+            return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
+        }))
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .on('error', notify.onError(function (error) {
+            return 'Babel failed: ' + error.message;
+        }))
+        .pipe(uglify())
+        .on('error', notify.onError(function (error) {
+            return 'Minification failed: ' + error.message;
+        }))
+        .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', function(callback) {
+gulp.task('default', function (callback) {
     gulp.watch('src/index.js', ['build']);
 });
